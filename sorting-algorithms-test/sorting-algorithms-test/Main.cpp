7d585727d83cabea37554dbc5729d31415b87fe2
Marek <marek.mikulski.14@gmail.com>
2019-02-12 21:33:24 +0100

    Adding Main.cpp

---
+++ sorting-algorithms-test/sorting-algorithms-test/Main.cpp
@@ -0,0 +1,12 @@
+// I'm using printf()s instead of couts because i try to get used to it. It works faster than couts and cins
+// try putting this: ios_base::sync_with_stdio(0); at the beggining of your code. It replaces couts with printf()s
+// same thing with scanf() and cin
+
+
+int main() {
+
+
+
+
+	return 0;
+}
